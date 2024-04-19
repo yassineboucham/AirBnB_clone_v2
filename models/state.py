6bed726054bd7489dd3b0f20910149642b3b378a
yassineboucham <yassineboucham11@gamil.com>
2024-04-19 02:24:45 +0100

    all

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import Base
+from model.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = column(string(128), nullable=False)
+    cities = relationship("City", cascade="delete", backref="State")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def cities(self):
+            """geter atrebute"""
+            list_City = []
+            all_City = model.storage.all(City)
+            for city in all_City.values():
+                if city.id == self.id:
+                    list_City.append(city)
+            return list_City
