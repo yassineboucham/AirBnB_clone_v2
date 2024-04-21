d1c2b35b7ef19978ff3c9ea5563f5c2379d97cca
yassineboucham <yassineboucham11@gamil.com>
2024-04-21 20:13:49 +0100

    h

--- models/state.py
+++ models/state.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-import Base
-from model.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
+import os
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = column(string(128), nullable=False)
-    cities = relationship("City", cascade="delete", backref="State")
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="delete", backref="state")
 
     if os.getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """geter atrebute"""
-            list_City = []
-            all_City = model.storage.all(City)
-            for city in all_City.values():
-                if city.id == self.id:
-                    list_City.append(city)
-            return list_City
+            """ cities getter attribute """
+            cit_lis = []
+            all_cit = models.storage.all(City)
+            for city in all_cit.values():  # change .items() to values() as it
+                # returns an obj that contains values of a dictionary as a list
+                if city.state_id == self.id:
+                    cit_lis.append(city)
+            return cit_lis
