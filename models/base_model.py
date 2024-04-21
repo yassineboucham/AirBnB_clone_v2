45113f88c2730334f77b149c658e08789e47326d
yassineboucham <yassineboucham11@gamil.com>
2024-04-21 20:10:58 +0100

    h

--- models/base_model.py
+++ models/base_model.py
@@ -2,30 +2,36 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = column(string(60), unique=True, primary_key=True, nullable=False)
-    created_at = column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = column(DateTime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), unique=True, primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if not kwargs or ("updated_at" not in kwargs and
+                          "created_at" not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            pass
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -40,11 +46,17 @@ class BaseModel:
         storage.new(self)
         storage.save()
 
+    def delete(self):
+        """Deletes an instance from storage"""
+        from models import storage
+        storage.delete(self)
+
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        #dele the key if not exict
+
+        # remove the key if it exists
         if (dictionary.get("_sa_instance_state")):
             dictionary.pop("_sa_instance_state")
 
@@ -53,8 +65,3 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-
-    def delete(self):
-        """ delete the current instance from the storage """
-        from model import storage
-        storage.delete(self)
