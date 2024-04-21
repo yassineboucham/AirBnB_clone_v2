65c7f1ecb8776957a114b8ec8e131684b2d6507f
yassineboucham <yassineboucham11@gamil.com>
2024-04-21 20:06:16 +0100

    db

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+import Base
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
