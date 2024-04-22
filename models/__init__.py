13436384d94af9d08400c1dbed8696af240a777a
yassineboucham <yassineboucham11@gamil.com>
2024-04-22 07:49:10 +0100

    3

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
+"""
+This module instantiates an object of storage depending on type
+Contains the model of all objects
+"""
+from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.db_storage import DBStorage
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
-    storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''The class for Amenity'''
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
