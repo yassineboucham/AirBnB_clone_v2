54e5931c4d78f92660445ce0c2ff48c99dce554d
yassineboucham <yassineboucham11@gamil.com>
2024-05-06 02:30:43 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,15 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
-
-apt-get update
-apt-get -y install nginx
-# Install Nginx if it not already installed
-mkdir -p /data/web_static/releases/test/
-# Create the folder /data/ if it doesn’t already exist
-# Create the folder /data/web_static/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-mkdir -p /data/web_static/shared/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-# Create a fake HTML file /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Create a symbolic link /data/web_static/current linked to the...
-# .../data/web_static/releases/test/ folder. If the symbolic link already...
-# ...exists, it should be deleted and recreated every time the script is ran.
-# chown -hR ubuntu:ubuntu /data/
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can
-# assume this user and group exist). This should be recursive; everything
-# inside should be created/owned by this user/group.
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static).
-# Don’t forget to restart Nginx after updating the configuration:
-service nginx restart
+# Script that configures Nginx server with some folders and files
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+conf="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "45i $conf" /etc/nginx/sites-available/default
+sudo service nginx start
+sudo service nginx restart
+sudo service nginx reload
