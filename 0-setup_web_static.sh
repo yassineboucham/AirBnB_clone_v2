870e7450551a61678fd76ce60530f12431a16aa1
yassineboucham <yassineboucham2002@gmail.com>
2024-08-18 23:00:13 +0100

    config nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
-#sets up your web servers for the deployment of web_static
+# Bash script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
-</head>
-<body>
-    <h1>Hello, World!</h1>
-</body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<h1>Hello Worlds</h1>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f "/data/web_static/releases/test/"  "/data/web_static/current"
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu  /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
