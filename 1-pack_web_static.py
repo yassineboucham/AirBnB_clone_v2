995e1ca1b6f71ffb66df85618afd09b49ee085ec
yassineboucham <yassineboucham2002@gmail.com>
2024-08-19 15:43:36 +0100

    let me test

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" script that generates a .tgz archive from the contents """
+
+
+from fabric.api import *
+import datetime
+
+def do_pack():
+    """archive from the contents of the web_static folder"""
+    local("sudo mkdir -p versions")
+    filename = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
