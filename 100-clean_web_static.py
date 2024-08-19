4fc78d8fc230303827dd5822c8ff72f09ca6afee
yassineboucham <yassineboucham2002@gmail.com>
2024-08-20 00:47:44 +0100

    edit ip

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['100.26.239.10', '52.91.127.91']
 
 
 def do_clean(number=0):
